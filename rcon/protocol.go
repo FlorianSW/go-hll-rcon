de0a7f0b618dfc03352bf664441f80fe45ec936a
Florian <florian.schmidt.welzow@t-online.de>
2023-11-15 19:40:08 +0100

    handle reconnect when server closes the connection unexpectedly

--- rcon/protocol.go
+++ rcon/protocol.go
@@ -7,6 +7,7 @@ import (
 	"net"
 	"strconv"
 	"strings"
+	"syscall"
 )
 
 const (
@@ -14,32 +15,42 @@ const (
 )
 
 var (
-	CommandFailed = errors.New("FAIL")
+	CommandFailed          = errors.New("FAIL")
+	ReconnectTriesExceeded = errors.New("there are no reconnects left")
 )
 
 type socket struct {
-	con net.Conn
-	key []byte
-	pw  string
+	con            net.Conn
+	key            []byte
+	pw             string
+	host           string
+	port           int
+	reconnectCount int
 }
 
-func newSocket(h string, p int, pw string) (*socket, error) {
+func makeConnection(h string, p int) (net.Conn, []byte, error) {
 	con, err := net.Dial("tcp4", fmt.Sprintf("%s:%d", h, p))
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	xorKey := make([]byte, MSGLEN)
 	_, err = con.Read(xorKey)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	xorKey = bytes.Trim(xorKey, "\x00")
+
+	return con, xorKey, err
+}
+
+func newSocket(h string, p int, pw string) (*socket, error) {
 	r := &socket{
-		con: con,
-		key: xorKey,
-		pw:  pw,
+		pw:             pw,
+		host:           h,
+		port:           p,
+		reconnectCount: 0,
 	}
-	return r, r.login()
+	return r, r.reconnect(nil)
 }
 
 func (r *socket) Close() error {
@@ -115,7 +126,35 @@ func (r *socket) command(cmd string) (string, error) {
 
 func (r *socket) write(cmd string) error {
 	_, err := r.con.Write(r.xor([]byte(cmd)))
+	if errors.Is(err, syscall.EPIPE) {
+		err = r.reconnect(err)
+		if err != nil {
+			return err
+		}
+		return r.write(cmd)
+	}
+	if err != nil {
+		r.resetReconnectCount()
+	}
 	return err
+}
+
+func (r *socket) reconnect(orig error) error {
+	if r.reconnectCount > 3 {
+		return ReconnectTriesExceeded
+	}
+	con, xorKey, err := makeConnection(r.host, r.port)
+	r.con = con
+	r.key = xorKey
+	if err != nil {
+		return fmt.Errorf("reconnect failed: %s, original error: %w", err.Error(), orig)
+	}
+	r.reconnectCount++
+	err = r.login()
+	if err != nil {
+		return fmt.Errorf("reconnect failed: %s, original error: %w", err.Error(), orig)
+	}
+	return nil
 }
 
 func (r *socket) read() ([]byte, error) {
@@ -123,8 +162,17 @@ func (r *socket) read() ([]byte, error) {
 	for {
 		rb := make([]byte, MSGLEN)
 		l, err := r.con.Read(rb)
+		if errors.Is(err, syscall.ECONNRESET) {
+			err = r.reconnect(err)
+			if err != nil {
+				return nil, err
+			}
+			l, err = r.con.Read(rb)
+		}
 		if err != nil {
 			return nil, err
+		} else {
+			r.resetReconnectCount()
 		}
 		rb = rb[:l]
 
@@ -146,4 +194,10 @@ func (r *socket) xor(b []byte) []byte {
 		msg = append(msg, mb)
 	}
 	return msg
+}
+
+func (r *socket) resetReconnectCount() {
+	if r.reconnectCount != 0 {
+		r.reconnectCount = 0
+	}
 }
