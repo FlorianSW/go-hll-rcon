d7d00ca407d23eb3346c4ca5bc11f28b1bc9697d
Florian <florian.schmidt.welzow@t-online.de>
2025-04-01 23:35:13 +0200

    update json field names and types according to documentation

--- rconv2/api/op_ServerInformation.go
+++ rconv2/api/op_ServerInformation.go
@@ -18,6 +18,32 @@ const (
 	PlayerPlatformSteam = PlayerPlatform("steam")
 )
 
+const (
+	PlayerTeamGer = iota
+	PlayerTeamUs
+	PlayerTeamRus
+	PlayerTeamGb
+	PlayerTeamDak
+	PlayerTeamB8a
+)
+
+const (
+	PlayerRoleRifleman = iota
+	PlayerRoleAssault
+	PlayerRoleAutomaticRifleman
+	PlayerRoleMedic
+	PlayerRoleSpotter
+	PlayerRoleSupport
+	PlayerRoleHeavyMachineGunner
+	PlayerRoleAntiTank
+	PlayerRoleEngineer
+	PlayerRoleOfficer
+	PlayerRoleSniper
+	PlayerRoleCrewman
+	PlayerRoleTankCommander
+	PlayerRoleArmyCommander
+)
+
 var (
 	requiresValue = []ServerInformationName{
 		ServerInformationNamePlayer,
@@ -41,37 +67,40 @@ func (s ServerInformation) Validate() error {
 }
 
 type GetPlayersResponse struct {
-	Players []GetPlayerResponse `json:"players"`
+	Players []GetPlayerResponse `json:"Players"`
 }
 
 type PlayerPlatform string
 
+type PlayerTeam int
+type PlayerRole int
+
 type GetPlayerResponse struct {
-	Id                   string         `json:"id"`
-	Platform             PlayerPlatform `json:"platform"`
-	Name                 string         `json:"name"`
-	ClanTag              string         `json:"clanTag"`
-	EpicOnlineServicesId string         `json:"eOSId"`
-	Level                int            `json:"level"`
-	Team                 string         `json:"team"`
-	Role                 string         `json:"role"`
-	Squad                string         `json:"platoon"`
-	Loadout              string         `json:"loadout"`
-	Kills                int            `json:"kills"`
-	Deaths               int            `json:"deaths"`
-	Score                ScoreData      `json:"scoreData"`
-	Position             WorldPosition  `json:"worldPosition"`
+	Id                   string         `json:"ID"`
+	Platform             PlayerPlatform `json:"Platform"`
+	Name                 string         `json:"Name"`
+	ClanTag              string         `json:"ClanTag"`
+	EpicOnlineServicesId string         `json:"EOSID"`
+	Level                int            `json:"Level"`
+	Team                 PlayerTeam     `json:"Team"`
+	Role                 PlayerRole     `json:"Role"`
+	Squad                string         `json:"Platoon"`
+	Loadout              string         `json:"Loadout"`
+	Kills                int            `json:"Kills"`
+	Deaths               int            `json:"Deaths"`
+	Score                ScoreData      `json:"ScoreData"`
+	Position             WorldPosition  `json:"WorldPosition"`
 }
 
 type ScoreData struct {
-	Combat    int `json:"cOMBAT"`
-	Offensive int `json:"offense"`
-	Defensive int `json:"defense"`
-	Support   int `json:"support"`
+	Combat    int `json:"COMBAT"`
+	Offensive int `json:"Offense"`
+	Defensive int `json:"Defense"`
+	Support   int `json:"Support"`
 }
 
 type WorldPosition struct {
-	X int `json:"x"`
-	Y int `json:"y"`
-	Z int `json:"z"`
+	X int `json:"X"`
+	Y int `json:"Y"`
+	Z int `json:"Z"`
 }
