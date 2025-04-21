f3174a8da5aeb4aa2ccb8589243b9d1418f5e1ab
Florian <florian.schmidt.welzow@t-online.de>
2025-04-21 02:39:26 +0200

    WIP: Locate player on the map grid
    The player WorldPosition now provides a function to locate the player on
    the map (as responded by SessionInfo) in a grid and numpad (e.g. B3 Numpad 5).
    
    This is still WIP, as the map center is different on some maps, which needs to be taken into account.
    
    Co-Authored-By: marcelhillesheim <contact@marcelhillesheim.com>

--- rconv2/api/op_ServerInformation.go
+++ rconv2/api/op_ServerInformation.go
@@ -117,12 +117,43 @@ func (w WorldPosition) IsSpawned() bool {
 	return (w.X + w.Y + w.Z) != 0
 }
 
-// Distance calculates the distance of this and another position in the game world. This includes movement on the x axis
-// (as represented in changed values of X and Y) as well as on the y axis (represented by changed Z values).
+// Distance calculates the distance of this and another position in the game world. This includes movement on the x-axis
+// (as represented in changed values of X and Y) as well as on the y-axis (represented by changed Z values).
 // This is calculated as if the distance was travelled in a straight line without observing obstacles. It depends on the
 // resolution of when the two involved positions were obtained how accurate the calculated distance is.
 func (w WorldPosition) Distance(o WorldPosition) Distance {
 	return Distance(math.Sqrt(math.Pow(w.X-o.X, 2) + math.Pow(w.Y-o.Y, 2) + math.Pow(w.Z-o.Z, 2)))
+}
+
+type DrawableMap interface {
+	mapData() *mapData
+}
+
+var (
+	xs     = []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"}
+	numpad = [][]int{
+		{7, 8, 9},
+		{4, 5, 6},
+		{1, 2, 3},
+	}
+)
+
+func (w WorldPosition) Grid(m DrawableMap) Grid {
+	d := m.mapData()
+	if d == nil || d.MapCenterOffset.X != 0 || d.MapCenterOffset.Y != 0 {
+		return Grid{}
+	}
+	xGrid, yGrid := math.Floor(w.X/d.SectorSize), math.Floor(w.Y/d.SectorSize)
+
+	xInGrid := w.X - xGrid*d.SectorSize
+	yInGrid := w.Y - yGrid*d.SectorSize
+	num := d.SectorSize / 3
+	return Grid{
+		// TODO breaks if the map center is different from the default 0,0 (see mapData)
+		X:      xs[int(xGrid)+5],
+		Y:      int(yGrid) + 6,
+		Numpad: numpad[int(math.Abs(math.Floor(yInGrid/num)))][int(math.Abs(math.Floor(xInGrid/num)))],
+	}
 }
 
 // Distance is supposed to be in centimeters (default unit of worlds in Unreal Engine)
@@ -155,6 +186,85 @@ type GetSessionResponse struct {
 	VIPQueueCount    int    `json:"vIPQueueCount"`
 }
 
+type Vector2D struct {
+	X float64
+	Y float64
+}
+
+type mapData struct {
+	SectorSize float64
+	// by default the center of the map is at vector 0,0, however, some maps (like Carentan Skirmish)
+	// move the center of the map (as visual to the player) on the x and/or y-axis. MapCenterOffset is the Vector
+	// that describes this offset. It has 0,0 by default.
+	MapCenterOffset Vector2D
+}
+
+// maps describe the default configuration of a map, or a specific different config if a map
+// is different to the default.
+var maps = map[string]map[string]mapData{
+	"Skirmish": {
+		"default": {
+			SectorSize: 13926,
+		},
+		"CARENTAN": {
+			SectorSize:      13926,
+			MapCenterOffset: Vector2D{X: 150, Y: -110},
+		},
+		"MORTAIN": {
+			SectorSize:      13926,
+			MapCenterOffset: Vector2D{X: 100, Y: 0},
+		},
+		"ST MARIE DU MONT": {
+			SectorSize:      13926,
+			MapCenterOffset: Vector2D{X: 0, Y: -27852.799},
+		},
+		"DRIEL": {
+			SectorSize:      13926,
+			MapCenterOffset: Vector2D{X: -20, Y: 28190},
+		},
+	},
+	"Warfare": {
+		// all older maps (SME, SMDM, etc) have a default sector width and height
+		"default": {
+			SectorSize: 19840,
+		},
+		// Carentan has a slightly higher sector size
+		"CARENTAN": {
+			SectorSize: 20160,
+		},
+		// newer maps have a 200x200m grid schema
+		"ELSENBORN RIDGE": {
+			SectorSize: 20000,
+		},
+		"MORTAIN": {
+			SectorSize: 20000,
+		},
+		"TOBRUK": {
+			SectorSize: 20000,
+		},
+	},
+}
+
+func (m GetSessionResponse) mapData() *mapData {
+	if mode, exists := maps[m.GameMode]; !exists {
+		return nil
+	} else if md, exists := mode[m.MapName]; exists {
+		return &md
+	} else if d, exists := mode["default"]; exists {
+		return &d
+	} else {
+		return nil
+	}
+}
+
+// GridSize returns the size in meters of a grid square on the map, depending on the current game mode.
+func (m GetSessionResponse) GridSize() float64 {
+	if d := m.mapData(); d != nil {
+		return d.SectorSize
+	}
+	return 0
+}
+
 type GetMapRotationResponse struct {
 	Maps []Map `json:"mAPS"`
 }
@@ -169,4 +279,14 @@ type Map struct {
 	TimeOfDay string `json:"timeOfDay"`
 	Id        string `json:"iD"`
 	Position  int    `json:"position"`
+}
+
+type Grid struct {
+	X      string
+	Y      int
+	Numpad int
+}
+
+func (g Grid) String() string {
+	return fmt.Sprintf("%s%d Numpad %d", g.X, g.Y, g.Numpad)
 }
